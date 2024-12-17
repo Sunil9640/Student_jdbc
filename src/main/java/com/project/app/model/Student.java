79c4f458221360eae11ab5f61b0509741c200a41
sunil <sunilyadav202496@gmail.com>
2024-12-17 11:31:48 +0530

    second commit Setter and getter

--- src/main/java/com/project/app/model/Student.java
+++ src/main/java/com/project/app/model/Student.java
@@ -1,15 +1,17 @@
 package com.project.app.model;
 
+import java.util.Objects;
+
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+//pojo class
 
-
-@Data
+/*@Data
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor*/
 @Component
 public class Student {
 	
@@ -18,6 +20,42 @@ public class Student {
 	private String name;
 	
 	private String department;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int sid, String name, String department) {
+		super();
+		this.sid = sid;
+		this.name = name;
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", name=" + name + ", department=" + department + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(department, name, sid);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(department, other.department) && Objects.equals(name, other.name) && sid == other.sid;
+	}
+	
+	
 
 
 	
